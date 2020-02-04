8df1d9aecc389bd24ac21ef516e03e7b843f1881
Drew Radcliff <sirampr@gmail.com>
2020-02-04 11:25:19 -0500

    Render Github User Info on the Page

--- src/App.js
+++ src/App.js
@@ -12,16 +12,32 @@ class App extends Component {
 
   // general method on App
   handleToggle = event => {
-    fetch(getGithubUserInfo("sirAMPR"))
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ user: data });
-      });
+    if (this.state.active === false) {
+      fetch(getGithubUserInfo("sirAMPR"))
+        .then(response => response.json())
+        .then(data => {
+          this.setState({ user: data, active: true });
+        });
+    } else {
+      this.setState({ active: false });
+    }
   };
 
   // lifecycle method on App
   render() {
-    return <button onClick={this.handleToggle}>Toggle User</button>;
+    return (
+      <>
+        <button onClick={this.handleToggle}>Toggle User</button>
+        {this.state.active && (
+          <>
+            <img src={this.state.user.avatar_url} alt="user profile" />
+            <h1>{this.state.user.name}</h1>
+            <p>{this.state.user.bio}</p>
+            <p>Followers: {this.state.user.followers}</p>
+          </>
+        )}
+      </>
+    );
   }
 }
 
